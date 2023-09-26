3c25a43cfccb4d2a98b1361ca28b1312554b955d
Grace-Bam <grabelbam@gmail.com>
2023-09-26 11:02:14 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - that gets the length of a prefix substring.
+ * @s: parameter 1
+ * @accept: parameter 2
+ *
+ * Return: s
+ */
+unsigned int _strspn(char *s, char *accept)
+
+{
+	char *s = p
+
+	while (*s)
+	{
+		q = accept;
+
+		while (q && q != *p)
+		{
+		p++;
+		}
+		if (i == '\0')
+		{
+			break;
+		}
+		q++;
+	}
+	return (q -p);
+}
+
